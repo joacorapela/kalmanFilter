7f342c05d297b3542bc8050b5f444a6b91ceec6b
Joaquin Rapela <joacorapela@gmail.com>
2020-06-28 15:51:45 +0100

    Dummy changes

--- scripts/doSquareRootLearnCircleWithEstimatedInitialCondFA.R
+++ scripts/doSquareRootLearnCircleWithEstimatedInitialCondFA.R
@@ -1,9 +1,12 @@
 
+require(MASS)
 require(pcaMethods)
 require(ramcmc)
 require(plotly)
 require(mvtnorm)
 require(gridExtra)
+source("../src/chol_downdate_higherOrder.R")
+source("../src/lsolve.R")
 source("../src/squareRootKF.R")
 source("../src/smoothLDS.R")
 source("../src/emEstimationSquareRootKF.R")
@@ -22,8 +25,8 @@ processAll <- function() {
     simRes <- get(load(simulationFilename))
     zs <- simRes$x
     zsForFA <- t(as.matrix(zs))
+    gamma0Var <- 10.0
     initialConds <- estimateKFInitialCondFA(z=zsForFA, nFactors=nFactors, rotation="none")
-
 
     A <- simRes$A
     C <- simRes$C
@@ -38,25 +41,25 @@ processAll <- function() {
     V0 <- simRes$V0
     SRSigmaX0 <- chol(x=V0)
 
-    A0 <- initialConds$A
+    A0 <- initialConds$A+rnorm(n=length(initialConds$A))*300.0
     # A0 <- A
-    Gamma0 <- initialConds$Gamma
+    Gamma0 <- gamma0Var*diag(rep(1, times=nrow(A0)))
     SRSigmaW0 <- chol(x=Gamma0)
     # C0 <- C+matrix(rnorm(n=length(C), sd=initialCondNoiseSD), ncol=ncol(C))
     # C0 <- initialConds$C
     C0 <- C
     B0 <- B
     D0 <- D
-    Sigma0 <- initialConds$Sigma
+    Sigma0 <- diag(initialConds$sigmaDiag)
     SRSigmaV0 <- chol(x=Sigma0)
     xHat00 <- xHat0
     V00 <- V0
     SRSigmaX00 <- chol(x=V00)
 
-    res <- squareRootKF(A=A, B=B, C=C, D=D, xHat0=xHat0, SRSigmaX0=SRSigmaX0, SRSigmaW=SRSigmaW, SRSigmaV=SRSigmaV, us=us, zs=zs)
+    res <- squareRootKF(A=A, B=B, C=C, D=D, x0=xHat0, SRSigmaX0=SRSigmaX0, SRSigmaW=SRSigmaW, SRSigmaV=SRSigmaV, us=us, zs=zs)
     logMsgPattern <- sprintf("loglik(iter=%%04d)=%%.4f (%.4f)", sum(log(res$c)))
 
-    eRes <- emEstimationSquareRootKF(zs=zs, A0=A0, SRSigmaW0=SRSigmaW0, C0=C0, SRSigmaV0=SRSigmaV0, B0=B0, D0=D0, xHat00=xHat00, SRSigmaX0=SRSigmaX00, nIter=nIter, tol=tol, varsToEstimate=list(initialStateMean=TRUE, initialStateCovariance=TRUE, transitionMatrix=TRUE, transitionCovariance=TRUE, observationMatrix=FALSE, observationCovariance=TRUE), logMsgPattern=logMsgPattern)
+    eRes <- emEstimationSquareRootKF(zs=zs, A0=A0, SRSigmaW0=SRSigmaW0, C0=C0, SRSigmaV0=SRSigmaV0, B0=B0, D0=D0, xHat00=xHat00, SRSigmaX0=SRSigmaX00, nIter=nIter, tol=tol, varsToEstimate=list(initialStateMean=TRUE, initialStateCovariance=TRUE, transitionMatrix=TRUE, transitionCovariance=TRUE, observationMatrix=FALSE, observationCovariance=TRUE), covsConstraints=list(SigmaX0="diagonal", SigmaW="diagonal", SigmaV="diagonal"), logMsgPattern=logMsgPattern)
 
     df <- data.frame(x=1:length(eRes$logLikelihoods), 
                      y=eRes$logLikelihoods)
@@ -66,10 +69,10 @@ processAll <- function() {
     p1 <- p1 + xlab("Time")
     p1 <- p1 + ylab("Log Likelihood")
 
-    fRes <- squareRootKF(A=eRes$A, B=B, C=eRes$C, D=D, xHat0=eRes$xHat0, SRSigmaX0=chol(x=eRes$V0), SRSigmaW=chol(x=eRes$Gamma), SRSigmaV=chol(x=eRes$Sigma), us=us, zs=zs)
-    sRes <- smoothLDS(A=eRes$A, mu=fRes$xHat, V=fRes$SigmaXHat, P=fRes$SigmaX[2:length(fRes$SigmaX)])
-    fRes0 <- squareRootKF(A=A0, B=B0, C=C0, D=D0, xHat0=xHat00, SRSigmaX0=chol(x=V00), SRSigmaW=chol(x=Gamma0), SRSigmaV=chol(x=Sigma0), us=us, zs=zs)
-    sRes0 <- smoothLDS(A=A0, mu=fRes0$xHat, V=fRes0$SigmaXHat, P=fRes0$SigmaX[2:length(fRes0$SigmaX)])
+    fRes <- squareRootKF(A=eRes$A, B=B, C=eRes$C, D=D, x0=eRes$x0, SRSigmaX0=chol(x=eRes$V0), SRSigmaW=chol(x=eRes$Gamma), SRSigmaV=chol(x=eRes$Sigma), us=us, zs=zs)
+    sRes <- smoothLDS(A=eRes$A, mu=fRes$x, V=fRes$SigmaXHat, P=fRes$SigmaX[2:length(fRes$SigmaX)])
+    fRes0 <- squareRootKF(A=A0, B=B0, C=C0, D=D0, x0=xHat00, SRSigmaX0=chol(x=V00), SRSigmaW=chol(x=Gamma0), SRSigmaV=chol(x=Sigma0), us=us, zs=zs)
+    sRes0 <- smoothLDS(A=A0, mu=fRes0$x, V=fRes0$SigmaXHat, P=fRes0$SigmaX[2:length(fRes0$SigmaX)])
 
     data <- data.frame()
     for(i in 1:nrow(simRes$z)) {
