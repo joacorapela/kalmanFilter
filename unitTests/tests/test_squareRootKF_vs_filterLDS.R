57d358b99c6825f7c9e5961716c553d359c42c4f
Joaquin Rapela <joacorapela@gmail.com>
2020-09-09 16:31:07 +0100

    Small changes; e.g., matched the new interface of squareRootKF using Holmes notation

--- unitTests/tests/test_squareRootKF_vs_filterLDS.R
+++ unitTests/tests/test_squareRootKF_vs_filterLDS.R
@@ -1,6 +1,7 @@
 
 require(mvtnorm)
 require(ramcmc)
+require(nlme)
 source("../../src/squareRootKF.R")
 source("../../src/lsolve.R")
 source("../../src/chol_downdate_higherOrder.R")
@@ -9,8 +10,8 @@ source("~/dev/research/programs/src/R/math/l2Norm.R")
 
 processAll <- function() {
     noiseSD <- 1e-4
-    tol <- 1e-6
-    initialStateType <- "init01"
+    tol <- 1e-2
+    stateType0 <- "init10"
     V0SD <- 1e4
     simulationFilename <- "../../scripts/results/simulationCircle.RData"
 
@@ -20,26 +21,25 @@ processAll <- function() {
     nObs <- ncol(simRes$x)
 
     obs <- simRes$x
-    us <- matrix(0, nrow=1, ncol=nObs)
     x0 <- simRes$mu0 + rnorm(n=nLatents, sd=noiseSD)
     V0 <- simRes$V0 + matrix(rnorm(n=nLatents^2, sd=noiseSD), nrow=nLatents)
     A0 <- simRes$A + matrix(rnorm(n=nLatents^2, sd=noiseSD), ncol=nLatents)
     Gamma0 <- simRes$Gamma + matrix(rnorm(n=nLatents^2, sd=noiseSD), ncol=nLatents)
     C0 <- simRes$C + matrix(rnorm(n=nNeurons*nLatents, sd=noiseSD), ncol=nLatents)
     Sigma0 <- simRes$Sigma + matrix(rnorm(n=nNeurons^2, sd=noiseSD), ncol=nNeurons)
-    B0 <- matrix(0, nrow=nLatents, ncol=1)
-    D0 <- matrix(0, nrow=nNeurons, ncol=1)
     x0 <- rep(0, times=nLatents)
     V0 <- V0SD^2*diag(rep(1, times=nLatents))
 
-    fRes_squaareRootKF <- squareRootKF(A=A0, B=B0, C=C0, D=D0, x0=x0, initialStateType=initialStateType, SRSigmaX0=chol(x=V0), SRSigmaW=chol(x=Gamma0), SRSigmaV=chol(x=Sigma0), us=us, zs=obs)
+    fRes_squareRootKF <- squareRootKF(B=A0, Z=C0, x0=x0, srV0=chol(x=V0), stateType0=stateType0, srQ=chol(x=Gamma0), srR=chol(x=Sigma0), ys=obs)
     fRes_filterLDS <- filterLDS(x=obs, A=A0, Gamma=Gamma0, C=C0, Sigma=Sigma0, mu0=x0, V0=V0)
 
     for(n in 1:nObs) {
-        diff <- fRes_squaareRootKF$x[,n]-fRes_filterLDS$z[,n]
+        diff <- fRes_squareRootKF$xnn[, 1, n]-fRes_filterLDS$mu[,n]
         error <- l2Norm(diff)
         if(error>tol) {
-            stop(sprintf("Error comparing states>%f", tol))
+            show(sprintf("Error comparing state[%d], error=%f>tol=%f", n, error, tol))
+        } else {
+            show(sprintf("Success comparing state[%d], error=%f<tol=%f", n, error, tol))
         }
     }
     show("Success")
