61ace190cf766b22b696f824715b8f2b9afca791
Joaquin Rapela <joacorapela@gmail.com>
2020-09-11 20:35:33 +0100

    Added simulation of state x0

--- src/simulateLDS.R
+++ src/simulateLDS.R
@@ -1,20 +1,21 @@
 require(MASS)
 
-simulateLDS <- function(nObs, B, Q, Z, R, x00, V00) {
-    ssDim <- nrow(B)
-    obsDim <- nrow(Z)
+simulateLDS <- function(N, B, Q, Z, R, mu0, V0) {
+    M <- nrow(B)
+    P <- nrow(Z)
     # state noise
-    w <- t(mvrnorm(n=nObs, mu=matrix(0, nrow=ssDim, ncol=1), Sigma=Q))
+    w <- t(mvrnorm(n=N, mu=matrix(0, nrow=M, ncol=1), Sigma=Q))
     # measurement noise
-    v <- t(mvrnorm(n=nObs, mu=matrix(0, nrow=obsDim, ncol=1), Sigma=R))
+    v <- t(mvrnorm(n=N, mu=matrix(0, nrow=P, ncol=1), Sigma=R))
     # initial state noise
-    x <- matrix(NaN, nrow=ssDim, ncol=nObs)
-    y <- matrix(NaN, nrow=obsDim, ncol=nObs)
-    x[,1] <- t(mvrnorm(n=1, mu=x00, Sigma=V00))
-    for(n in 2:nObs) {
+    x <- matrix(NaN, nrow=M, ncol=N)
+    y <- matrix(NaN, nrow=P, ncol=N)
+    x0 <- matrix(mvrnorm(n=1, mu=mu0, Sigma=V0), nrow=M)
+    x[,1] <- B%*%x0+w[,1]
+    for(n in 2:N) {
         x[,n] <- B%*%x[,n-1]+w[,n]
     }
     y <- Z%*%x+v
-    answer <- list(x=x, y=y)
+    answer <- list(x0=x0, x=x, y=y)
     return(answer)
 }
