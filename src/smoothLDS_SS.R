2488f35cfc2a4d5dd667f0d087ed38b68eec7844
Joaquin Rapela <joacorapela@gmail.com>
2020-09-14 14:21:02 +0100

    Renamed mu0 to m0

--- src/smoothLDS_SS.R
+++ src/smoothLDS_SS.R
@@ -1,4 +1,4 @@
-smoothLDS_SS <- function(B, xnn, Vnn, xnn1, Vnn1, mu0, V0) {
+smoothLDS_SS <- function(B, xnn, Vnn, xnn1, Vnn1, m0, V0) {
     N <- dim(xnn)[3]
     M <- nrow(B)
     xnN <- array(NA, dim=c(M, 1, N))
@@ -15,7 +15,7 @@ smoothLDS_SS <- function(B, xnn, Vnn, xnn1, Vnn1, mu0, V0) {
     # initial state x00 and V00
     # return the smooth estimates of the state at time 0: x0N and V0N
     J0 <- V0%*%t(B)%*%solve(Vnn1[,,1])
-    x0N <- mu0+J0%*%(xnN[,,1]-xnn1[,,1])
+    x0N <- m0+J0%*%(xnN[,,1]-xnn1[,,1])
     V0N <- V0+J0%*%(VnN[,,1]-Vnn1[,,1])%*%t(J0)
     answer <- list(xnN=xnN, VnN=VnN, Jn=Jn, x0N=x0N, V0N=V0N, J0=J0)
     # browser()
