33a5ef84f08900113c78f70df7f93e75fb6a6710
Joaquin Rapela <joacorapela@gmail.com>
2020-09-14 14:17:29 +0100

    Renamed C to Z A to B, etc

--- src/estimateKFInitialCondPPCA.R
+++ src/estimateKFInitialCondPPCA.R
@@ -4,14 +4,12 @@ require(MASS)
 estimateKFInitialCondPPCA <- function(z, nFactors) {
     # res <- factanal(~z, factors=nFactors, rotation="varimax", na.action=na.exclude)
     res <- pca(z, method="ppca", nPcs=nFactors)
-    C <- loadings(res)
+    Z <- loadings(res)
     latents <- scores(res)
-    Sigma <- diag(nrow(C))*1e-2
     XnextT <- latents[2:nrow(latents),]
     XcurT <- latents[1:(nrow(latents)-1),]
     # lmRes <- lm(XnextT~0+XcurT)
     # A <- solve(a=t(XcurT)%*%XcurT, b=t(XcurT)%*%XnextT)
-    A <- t(ginv(XcurT)%*%XnextT)
-    Gamma <- diag(nrow(A))*1e-2
-    return(list(A=A, Gamma=Gamma, C=C, Sigma=Sigma))
+    B <- t(ginv(XcurT)%*%XnextT)
+    return(list(B=B, Z=Z))
 }
