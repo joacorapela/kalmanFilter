de9b16292e86649b2c0cc245b51863f98d8f4ab6
Joaquin Rapela <joacorapela@gmail.com>
2020-09-15 21:27:17 +0100

    Several minor changes

--- scripts/doSimulateLDSMultiTrial.R
+++ scripts/doSimulateLDSMultiTrial.R
@@ -2,27 +2,31 @@ require(ini)
 source("../src/simulateLDS.R")
 
 processAll <- function() {
-    simConfigFilename <- "data/00000001_simulation_metaData.ini"
-    simFilenamePattern <- "results/%08d_simulation.RData"
+    simConfigNumber <- 1
+    simConfigFilenamePattern <- "data/%08d_simulation_metaData.ini"
+    simResMetaDataFilenamePattern <- "results/%08d_simulation.ini"
+    simResFilenamePattern <- "results/%08d_simulation.RData"
 
+    simConfigFilename <- sprintf(simConfigFilenamePattern, simConfigNumber)
     simConfig <- read.ini(simConfigFilename)
 
     exit <- FALSE
     while(!exit) {
         simResNumber <- sample(1e8, 1)
-        simFilename <- sprintf(simFilenamePattern, simResNumber)
-        if(!file.exists(simFilename)) {
+        simResFilename <- sprintf(simResFilenamePattern, simResNumber)
+        if(!file.exists(simResFilename)) {
             exit <- TRUE
         }
     }
-    show(sprintf("Simulation results in: %s", simFilename))
+    simResMetaDataFilename <- sprintf(simResMetaDataFilenamePattern, simResNumber)
+    show(sprintf("Simulation results in: %s", simResFilename))
 
     # sampling rate
     sRate <- as.double(simConfig$control_variables$sRate)
     dt <- 1/sRate
 
     # number of observations
-    nObs <- as.numeric(simConfig$control_variables$nObs)
+    N <- as.numeric(simConfig$control_variables$N)
 
     # number of trials
     nTrials <- as.numeric(simConfig$control_variables$nTrials)
@@ -35,20 +39,20 @@ processAll <- function() {
     Q <- eval(parse(text=simConfig$state_variables$Q))
 
     # initial state mean
-    x00Trial1 <- eval(parse(text=simConfig$initial_state_variables$x00Trial1))
-    stateDim <- length(x00Trial1)
-    x00 <- array(dim=c(nTrials, stateDim))
-    x00[1,] <- x00Trial1
+    m0Trial1 <- eval(parse(text=simConfig$initial_state_variables$m0Trial1))
+    stateDim <- length(m0Trial1)
+    m0 <- array(dim=c(nTrials, stateDim))
+    m0[1,] <- m0Trial1
     for(i in 2:nTrials) {
-        x00[i,] <- eval(parse(text=simConfig$initial_state_variables[[sprintf("x00Trial%d", i)]]))
+        m0[i,] <- eval(parse(text=simConfig$initial_state_variables[[sprintf("m0Trial%d", i)]]))
     }
 
     # initial state covariance
-    V00Trial1 <- eval(parse(text=simConfig$initial_state_variables$V00Trial1))
-    V00 <- array(dim=c(nTrials, stateDim, stateDim))
-    V00[1,,] <- V00Trial1
+    V0Trial1 <- eval(parse(text=simConfig$initial_state_variables$V0Trial1))
+    V0 <- array(dim=c(nTrials, stateDim, stateDim))
+    V0[1,,] <- V0Trial1
     for(i in 2:nTrials) {
-        V00[i,,] <- eval(parse(text=simConfig$initial_state_variables[[sprintf("V00Trial%d", i)]]))
+        V0[i,,] <- eval(parse(text=simConfig$initial_state_variables[[sprintf("V0Trial%d", i)]]))
     }
 
     # state-measurement transfer
@@ -57,17 +61,21 @@ processAll <- function() {
     # measurements noise covariance
     R <- eval(parse(text=simConfig$measurements_variables$R))
 
-    dObs <- nrow(Z)
-    dLat <- ncol(Z)
-    x <- array(dim=c(nTrials, dLat, nObs))
-    y <- array(dim=c(nTrials, dObs, nObs))
+    P <- nrow(Z)
+    M <- ncol(Z)
+    x <- array(dim=c(nTrials, M, N))
+    y <- array(dim=c(nTrials, P, N))
     for(i in 1:nTrials) {
-        res <- simulateLDS(nObs=nObs, B=B, Q=Q, x00=x00[i,], V00=V00[i,,], Z=Z, R=R)
+        res <- simulateLDS(N=N, B=B, Q=Q, m0=m0[i,], V0=V0[i,,], Z=Z, R=R)
         x[i,,] <- res$x
         y[i,,] <- res$y
     }
-    simRes <- list(x=x, y=y, B=B, Q=Q, x00=x00, V00=V00, Z=Z, R=R)
-    save(simRes, file=simFilename)
+    simRes <- list(x=x, y=y, B=B, Q=Q, m0=m0, V0=V0, Z=Z, R=R)
+    save(simRes, file=simResFilename)
+
+    metaData <- list()
+    metaData[["simulation_info"]] <- list(simConfigNumber=simConfigNumber)
+    write.ini(x=metaData, filepath=simResMetaDataFilename)
 
     browser()
 }
