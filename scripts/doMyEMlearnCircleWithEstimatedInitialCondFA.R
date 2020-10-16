5a164dee2dd28eb135b26a67dafee1377e758306
Joaquin Rapela <joacorapela@gmail.com>
2020-10-16 19:36:22 +0100

    Renamed _R to _SS

--- scripts/doMyEMlearnCircleWithEstimatedInitialCondFA.R
+++ scripts/doMyEMlearnCircleWithEstimatedInitialCondFA.R
@@ -2,23 +2,21 @@
 require(MASS)
 require(reshape2)
 require(plotly)
-source("../src/emEstimationKF_R.R")
-source("../src/filterLDS_R.R")
-source("../src/smoothLDS_R.R")
-source("../src/squareRootKF.R")
-source("../src/smoothLDS_B.R")
+source("../src/emEstimationKF_SS.R")
+source("../src/filterLDS_SS.R")
+source("../src/smoothLDS_SS.R")
 source("../src/estimateKFInitialCondFA.R")
 source("../src/estimateKFInitialCondPPCA.R")
-source("../src/plotTrueInitialAndEstimatedMatrices.R")
-source("../src/plotTrueInitialAndEstimatedVectors.R")
+# source("../src/plotTrueInitialAndEstimatedMatrices.R")
+# source("../src/plotTrueInitialAndEstimatedVectors.R")
 
 processAll <- function() {
     dimObs <- 2*1
     nFactors <- 2
     tol <- 1e-6
     V0Var <- 1e1
-    gamma0Var <- 1e1
-    sigma0Var <- 1e1
+    Q0Var <- 1e1
+    R0Var <- 1e1
     maxIter <- 100
     stateType0 <- "init00"
     # simulationFilename <- "results/simulationCircle.RData"
@@ -35,20 +33,22 @@ processAll <- function() {
     # initialConds <- estimateKFInitialCondFA(z=t(as.matrix(zs)), nFactors=nFactors)
     initialConds <- estimateKFInitialCondPPCA(z=t(as.matrix(zs)), nFactors=nFactors)
 
-    A0 <- initialConds$A
-    Gamma0 <- gamma0Var*diag(rep(1, ncol(A0)))
-    C0 <- initialConds$C
-    # C0 <- simRes$C
-    Sigma0 <- sigma0Var*diag(rep(1, times=dimObs))
+    B0 <- initialConds$B
+    Q0 <- Q0Var*diag(rep(1, ncol(B0)))
+    Z0 <- initialConds$Z
+    # Z0 <- simRes$C
+    R0 <- R0Var*diag(rep(1, times=dimObs))
     x0 <- simRes$mu0
     V0 <- V0Var*diag(rep(1, times=nFactors))
 
-    res_emEstimationKF_R <- emEstimationKF_R(ys=zs, B0=initialConds$A, Q0=Gamma0, Z0=initialConds$C, R0=Sigma0, x00=x0, V00=V0, maxNIter=maxIter, tol=tol, stateType0=stateType0, varsToEstimate=list(initialStateMean=TRUE, initialStateCovariance=TRUE, transitionMatrix=TRUE, transitionCovariance=TRUE, observationMatrix=TRUE, observationCovariance=TRUE), checkSymmetry=FALSE, checkPD=FALSE)
+    res_emEstimationKF_SS <- emEstimationKF_SS(y=zs, B0=initialConds$A, Q0=Q0, Z0=initialConds$C, R0=R0, x0=x0, V0=V0, maxIter=maxIter, tol=tol, stateType0=stateType0, varsToEstimate=list(initialStateMean=TRUE, initialStateCovariance=TRUE, transitionMatrix=TRUE, transitionCovariance=TRUE, observationMatrix=TRUE, observationCovariance=TRUE), checkSymmetry=FALSE, checkPD=FALSE)
 
-    save(file=resultsFilename, res_emEstimationKF_R)
+    save(file=resultsFilename, res_emEstimationKF_SS)
+
+browser()
 
     logLikFigFilename <- "figures/circleMmyEM_logLik.html"
-    logLik <- res_emEstimationKF_R$logLike
+    logLik <- res_emEstimationKF_SS$logLike
     df <- data.frame(x=1:length(logLik), y=logLik)
     p <- ggplot(data=df, mapping=aes(x=x, y=y))
     p <- p + geom_point() + geom_line()
@@ -57,35 +57,35 @@ processAll <- function() {
     print(p)
 
     AFigFilename <- "figures//circleMyEM_A.html"
-    plotTrueInitialAndEstimatedMatrices(true=simRes$A, initial=initialConds$A, estimated=res_emEstimationKF_R$B, title="B", figFilename=AFigFilename)
+    plotTrueInitialAndEstimatedMatrices(true=simRes$A, initial=initialConds$A, estimated=res_emEstimationKF_SS$B, title="B", figFilename=AFigFilename)
 
     CFigFilename <- "figures//circleMyEM_C.html"
-    plotTrueInitialAndEstimatedMatrices(true=simRes$C, initial=initialConds$C, estimated=res_emEstimationKF_R$Z, title="Z", figFilename=CFigFilename)
+    plotTrueInitialAndEstimatedMatrices(true=simRes$C, initial=initialConds$C, estimated=res_emEstimationKF_SS$Z, title="Z", figFilename=CFigFilename)
 
-    GammaFigFilename <- "figures//circleMyEM_varGamma.html"
-    plotTrueInitialAndEstimatedMatrices(true=simRes$Gamma, estimated=res_emEstimationKF_R$Q, title="Gamma", figFilename=GammaFigFilename)
+    QFigFilename <- "figures//circleMyEM_varQ.html"
+    plotTrueInitialAndEstimatedMatrices(true=simRes$Q, estimated=res_emEstimationKF_SS$Q, title="Q", figFilename=QFigFilename)
 
-    SigmaFigFilename <- "figures//circleMyEM_Sigma.html"
-    plotTrueInitialAndEstimatedMatrices(true=simRes$Sigma, estimated=res_emEstimationKF_R$R, title="Sigma", figFilename=SigmaFigFilename)
+    RFigFilename <- "figures//circleMyEM_R.html"
+    plotTrueInitialAndEstimatedMatrices(true=simRes$R, estimated=res_emEstimationKF_SS$R, title="R", figFilename=RFigFilename)
 
     x0FigFilename <- "figures//circleMyEM_x0.html"
-    plotTrueInitialAndEstimatedVectors(true=simRes$mu0, estimated=res_emEstimationKF_R$x0, title="x0", figFilename=x0FigFilename)
+    plotTrueInitialAndEstimatedVectors(true=simRes$mu0, estimated=res_emEstimationKF_SS$x0, title="x0", figFilename=x0FigFilename)
 
     V0FigFilename <- "figures//circleMyEM_varV0.html"
-    plotTrueInitialAndEstimatedMatrices(estimated=res_emEstimationKF_R$V0, title="V0", figFilename=V0FigFilename)
+    plotTrueInitialAndEstimatedMatrices(estimated=res_emEstimationKF_SS$V0, title="V0", figFilename=V0FigFilename)
 
     # start run KF and KS using with initial parameters
     checkSymmetry <- FALSE
     symmetryTol <- 1e-4
     checkPD <- FALSE
     pdTol <- 1e-4
-    fRes0 <- filterLDS_R(B=A0, Z=C0, x0=x0, V0=V0, stateType0=stateType0, Q=Gamma0, R=Sigma0, ys=zs, checkSymmetry=checkSymmetry, symmetryTol=symmetryTol, checkPD=checkPD, pdTol=pdTol)
-    sRes0 <- smoothLDS_R(B=A0, xnn=fRes0$xnn, Vnn=fRes0$Vnn, xnn1=fRes0$xnn1, Vnn1=fRes0$Vnn1, stateType0=stateType0, x00=x0, V00=V0)
+    fRes0 <- filterLDS_SS(B=B0, Z=Z0, x0=x0, V0=V0, stateType0=stateType0, Q=Q0, R=R0, ys=zs, checkSymmetry=checkSymmetry, symmetryTol=symmetryTol, checkPD=checkPD, pdTol=pdTol)
+    sRes0 <- smoothLDS_SS(B=B0, xnn=fRes0$xnn, Vnn=fRes0$Vnn, xnn1=fRes0$xnn1, Vnn1=fRes0$Vnn1, stateType0=stateType0, x00=x0, V00=V0)
     # end run KF and KS using with initial conditions
 
     # start run KF and KS using with estimated parameters
-    fRes <- filterLDS_R(B=res_emEstimationKF_R$B, Z=res_emEstimationKF_R$Z, x0=res_emEstimationKF_R$x0, V0=res_emEstimationKF_R$V0, stateType0=stateType0, Q=res_emEstimationKF_R$Q, R=res_emEstimationKF_R$R, ys=zs, checkSymmetry=checkSymmetry, symmetryTol=symmetryTol, checkPD=checkPD, pdTol=pdTol)
-    sRes <- smoothLDS_R(B=res_emEstimationKF_R$B, xnn=fRes$xnn, Vnn=fRes$Vnn, xnn1=fRes$xnn1, Vnn1=fRes$Vnn1, stateType0=stateType0, x00=res_emEstimationKF_R$x0, V00=res_emEstimationKF_R$V0)
+    fRes <- filterLDS_SS(B=res_emEstimationKF_SS$B, Z=res_emEstimationKF_SS$Z, x0=res_emEstimationKF_SS$x0, V0=res_emEstimationKF_SS$V0, stateType0=stateType0, Q=res_emEstimationKF_SS$Q, R=res_emEstimationKF_SS$R, ys=zs, checkSymmetry=checkSymmetry, symmetryTol=symmetryTol, checkPD=checkPD, pdTol=pdTol)
+    sRes <- smoothLDS_SS(B=res_emEstimationKF_SS$B, xnn=fRes$xnn, Vnn=fRes$Vnn, xnn1=fRes$xnn1, Vnn1=fRes$Vnn1, stateType0=stateType0, x00=res_emEstimationKF_SS$x0, V00=res_emEstimationKF_SS$V0)
     # end run KF and KS using with estimated parameters
 
 
