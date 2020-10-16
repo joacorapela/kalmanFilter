d28992238017a8f9948b696270e7b6b1a497afa3
Joaquin Rapela <joacorapela@gmail.com>
2020-10-16 19:50:32 +0100

    Fixed incorrect state update equation

--- src/smoothLDS_SS_withOffsetsAndInputs.R
+++ src/smoothLDS_SS_withOffsetsAndInputs.R
@@ -13,7 +13,8 @@ smoothLDS_SS_withOffsetsAndInputs <- function(B, u, C, c, Q, xnn, Vnn, xnn1, Vnn
     VnN[,,nObs] <- Vnn[,,nObs]
     for(n in nObs:2) {
         Jn[,,n-1] <- t(solve(Vnn1[,,n], B%*%Vnn[,,n-1]))
-        xnN[,,n-1] <- xnn[,,n-1]+Jn[,,n-1]%*%(xnN[,,n]-xnn1[,,n])-(Vnn[,,n-1]-Jn[,,n-1]%*%Vnn1[,,n]%*%t(Jn[,,n-1]))%*%t(B)%*%solve(Q,u+C%*%c[,,n])
+        # xnN[,,n-1] <- xnn[,,n-1]+Jn[,,n-1]%*%(xnN[,,n]-xnn1[,,n])-(Vnn[,,n-1]-Jn[,,n-1]%*%Vnn1[,,n]%*%t(Jn[,,n-1]))%*%t(B)%*%solve(Q,u+C%*%c[,,n])
+        xnN[,,n-1] <- xnn[,,n-1]+Jn[,,n-1]%*%(xnN[,,n]-xnn1[,,n])
         VnN[,,n-1] <- Vnn[,,n-1]+Jn[,,n-1]%*%(VnN[,,n]-Vnn1[,,n])%*%t(Jn[,,n-1])
     }
     if(initStateAt==1) {
@@ -26,7 +27,8 @@ smoothLDS_SS_withOffsetsAndInputs <- function(B, u, C, c, Q, xnn, Vnn, xnn1, Vnn
             # initial state m0 and V0
             # return the smooth estimates of the state at time 0: x0N and V0N
             J0 <- t(solve(Vnn1[,,1], B%*%V0))
-            x0N <- m0+J0%*%(xnN[,,1]-xnn1[,,1])-(V0-J0%*%Vnn1[,,1]%*%t(J0))%*%t(B)%*%solve(Q, u+C%*%c[,,1])
+            # x0N <- m0+J0%*%(xnN[,,1]-xnn1[,,1])-(V0-J0%*%Vnn1[,,1]%*%t(J0))%*%t(B)%*%solve(Q, u+C%*%c[,,1])
+            x0N <- m0+J0%*%(xnN[,,1]-xnn1[,,1])
             V0N <- V0+J0%*%(VnN[,,1]-Vnn1[,,1])%*%t(J0)
             answer <- list(xnN=xnN, VnN=VnN, Jn=Jn, x0N=x0N, V0N=V0N, J0=J0)
             # browser()
