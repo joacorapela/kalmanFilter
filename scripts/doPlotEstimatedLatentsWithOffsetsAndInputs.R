1b6f50a8ca6695149669c5a0881196be10880211
Joaquin Rapela <joacorapela@gmail.com>
2020-10-16 19:41:45 +0100

    Trivial change

--- scripts/doPlotEstimatedLatentsWithOffsetsAndInputs.R
+++ scripts/doPlotEstimatedLatentsWithOffsetsAndInputs.R
@@ -10,7 +10,8 @@ processAll <- function() {
     # estResNumber <- 99485472
     # estResNumber <- 67957061
     # estResNumber <- 93973929
-    estResNumber <- 59934313
+    # estResNumber <- 59934313
+    estResNumber <- 27646949
     simFilenamePattern <- "results/%08d_simulation.RData"
     estResFilenamePattern <- "results/%08d_estimation.RData"
     estConfigFilenamePattern <- "data/%08d_estimation_metaData.ini"
