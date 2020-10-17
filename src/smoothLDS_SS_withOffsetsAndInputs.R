e876eeb12da7dc6f669b04e60c93630965135e0b
Joaquin Rapela <joacorapela@gmail.com>
2020-10-17 21:19:08 +0100

    Fixed derivation; smoothLDS_SS_withOffsetsAndInputs is equal to smoothLDS_SS; will remove smoothLDS_SS_withOffsetsAndInputs next

--- src/smoothLDS_SS_withOffsetsAndInputs.R
+++ src/smoothLDS_SS_withOffsetsAndInputs.R
@@ -1,4 +1,4 @@
-smoothLDS_SS_withOffsetsAndInputs <- function(B, u, C, c, Q, xnn, Vnn, xnn1, Vnn1, initStateAt=0, m0=NA, V0=NA) {
+smoothLDS_SS_withOffsetsAndInputs <- function(B, xnn, Vnn, xnn1, Vnn1, initStateAt=0, m0=NA, V0=NA) {
     if(initStateAt==1 && (!is.na(m0) || !is.na(V0)))
         warning("m0 and V0 are not used when initStateAt==1")
     if(initStateAt==0 && (is.na(m0) || is.na(V0)))
@@ -13,8 +13,7 @@ smoothLDS_SS_withOffsetsAndInputs <- function(B, u, C, c, Q, xnn, Vnn, xnn1, Vnn
     VnN[,,nObs] <- Vnn[,,nObs]
     for(n in nObs:2) {
         Jn[,,n-1] <- t(solve(Vnn1[,,n], B%*%Vnn[,,n-1]))
-        # xnN[,,n-1] <- xnn[,,n-1]+Jn[,,n-1]%*%(xnN[,,n]-xnn1[,,n])-(Vnn[,,n-1]-Jn[,,n-1]%*%Vnn1[,,n]%*%t(Jn[,,n-1]))%*%t(B)%*%solve(Q,u+C%*%c[,,n])
-        xnN[,,n-1] <- xnn[,,n-1]+Jn[,,n-1]%*%(xnN[,,n]-xnn1[,,n])
+        xnN[,,n-1] <- xnn[,,n-1]+Jn[,,n-1]%*%(xnN[,,n]-B%*%xnn[,,n-1])
         VnN[,,n-1] <- Vnn[,,n-1]+Jn[,,n-1]%*%(VnN[,,n]-Vnn1[,,n])%*%t(Jn[,,n-1])
     }
     if(initStateAt==1) {
@@ -27,8 +26,7 @@ smoothLDS_SS_withOffsetsAndInputs <- function(B, u, C, c, Q, xnn, Vnn, xnn1, Vnn
             # initial state m0 and V0
             # return the smooth estimates of the state at time 0: x0N and V0N
             J0 <- t(solve(Vnn1[,,1], B%*%V0))
-            # x0N <- m0+J0%*%(xnN[,,1]-xnn1[,,1])-(V0-J0%*%Vnn1[,,1]%*%t(J0))%*%t(B)%*%solve(Q, u+C%*%c[,,1])
-            x0N <- m0+J0%*%(xnN[,,1]-xnn1[,,1])
+            x0N <- m0+J0%*%(xnN[,,1]-B%*%m0)
             V0N <- V0+J0%*%(VnN[,,1]-Vnn1[,,1])%*%t(J0)
             answer <- list(xnN=xnN, VnN=VnN, Jn=Jn, x0N=x0N, V0N=V0N, J0=J0)
             # browser()
