c9669e0c0f0bd6e8eade24b3c517dbf82b4cc411
Joaquin Rapela <joacorapela@gmail.com>
2021-08-25 18:56:58 +0100

    Fixed serious problem. In the previous version I was introducing a delay of one sample to the stimuli

--- src/buildStimHist.R
+++ src/buildStimHist.R
@@ -1,9 +1,11 @@
 buildStimHist <- function(stim, memory) {
     buildInputsBlock <- function(stim, memory) {
-        inputs <- c()
-        N <- length(stim)
-        for(i in 1:(memory)) {
-            inputs <- rbind(inputs, c(rep(0, times=i), stim[1:(N-i)]))
+        inputs <- matrix(stim, nrow=1)
+        if(memory>0) {
+            N <- length(stim)
+            for(i in 1:(memory)) {
+                inputs <- rbind(inputs, c(rep(0, times=i), stim[1:(N-i)]))
+            }
         }
         return(inputs)
     }
