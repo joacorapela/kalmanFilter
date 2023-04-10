4b2ce489309bad37aa507085b1bd9ddd80a2f137
Joaquin Rapela <joacorapela@gmail.com>
2023-04-10 16:32:46 +0000

    Adding control input and changed naming (e.g., A to B, C to Z)

--- src/estimateKFInitialCondFA.R
+++ src/estimateKFInitialCondFA.R
@@ -1,12 +1,12 @@
-estimateKFInitialCondFA <- function(z, nFactors, rotation="varimax", scores="regression") {
-    res <- factanal(z, factors=nFactors, rotation=rotation, scores=scores, trace=TRUE)
-    C <- as(res$loadings, "matrix")
+estimateKFInitialCondFA <- function(z, nFactors, rotation="varimax", scores="regression", control=list(trace=TRUE, nstart=5)) {
+    res <- factanal(z, factors=nFactors, rotation=rotation, scores=scores, control=control)
+    Z <- as(res$loadings, "matrix")
     latents <- res$scores
-    sigmaDiag <- res$uniqueness
+    RDiag <- res$uniqueness
     XnextT <- latents[2:nrow(latents),]
     XcurT <- latents[1:(nrow(latents)-1),]
     # lmRes <- lm(XnextT~0+XcurT)
-    # A <- solve(a=t(XcurT)%*%XcurT, b=t(XcurT)%*%XnextT)
-    A <- t(ginv(XcurT)%*%XnextT)
-    return(list(A=A, C=C, sigmaDiag=sigmaDiag))
+    # B <- solve(a=t(XcurT)%*%XcurT, b=t(XcurT)%*%XnextT)
+    B <- t(ginv(XcurT)%*%XnextT)
+    return(list(B=B, Z=Z, RDiag=RDiag))
 }
