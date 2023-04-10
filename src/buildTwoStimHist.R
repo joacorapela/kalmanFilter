d0616b4af6f92d9a5845d37b85ff78b122e885df
Joaquin Rapela <joacorapela@gmail.com>
2023-04-10 16:29:06 +0000

    Substantial implemnetation change

--- src/buildTwoStimHist.R
+++ src/buildTwoStimHist.R
@@ -1,15 +1,15 @@
-buildStimHist <- function(stim, memory) {
+buildTwoStimHist <- function(stim, memory) {
     buildInputsBlock <- function(stim, memory) {
-        inputs <- matrix(stim, nrow=1)
-        if(memory>0) {
-            N <- length(stim)
-            for(i in 1:(memory)) {
-                inputs <- rbind(inputs, c(rep(0, times=i), stim[1:(N-i)]))
-            }
+        inputs <- c()
+        N <- length(stim)
+        for(i in 0:(memory)) {
+            inputs <- rbind(inputs, c(rep(0, times=i), stim[1:(N-i)]))
         }
         return(inputs)
     }
 
-    stimHist <- buildInputsBlock(stim=stim, memory=memory)
+    stim1Hist <- buildInputsBlock(stim=stim[1,], memory=memory)
+    stim2Hist <- buildInputsBlock(stim=stim[2,], memory=memory)
+    stimHist <- rbind(stim1Hist, stim2Hist)
     return(stimHist)
 }
